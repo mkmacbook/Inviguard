6fc32fdbbca4b20faecc663cd842b4a2f4f2a4cf
SoYeon Park_frontend.git <psy46500@naver.com>
2025-04-29 04:01:25 +0900

    스플래시 뷰 완료

---
+++ app/src/main/java/com/example/inviguard/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.inviguard;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // 2초 후 MainActivity로 이동
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
